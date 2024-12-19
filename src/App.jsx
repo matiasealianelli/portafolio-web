d25f6aa1a8583d07cdfc9af2304b42f662215daf
matiasealianelli <hola.matiasalianelli@gmail.com>
2024-12-19 19:58:25 -0300

    Agregue estructura jsx del Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Proyects from "./components/Proyects";
+
+
 function App() {
   return (
-    
+    <>
+      <Header />
+      <Hero />
+      <div className="contSections">
+        <Proyects />
+      </div>
+    </>
   );
 }
+
 export default App;
