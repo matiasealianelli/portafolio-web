974492f3afb7af76e34182dfe98165aa16ea80cb
matiasealianelli <hola.matiasalianelli@gmail.com>
2024-12-19 22:46:06 -0300

    Agregue estructura jsx del Componente Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Hero />
+    </>
   );
-};
+}
 
 export default App;
