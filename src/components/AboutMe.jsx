0e6f5c3eb9ce9fb5ebf86303fe277322079d5f03
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-01-20 13:05:02 -0300

    Agregue estructura jsx de seccion Sobre Mi

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,19 @@ import "../styles/AboutMe.css";
 
 function AboutMe() {
     return <>
-    
+    <section className="sections sobreMi" id="sobreMi">
+        <h2 className="title titleSections">Sobre mi</h2>
+        <div className="contSobreMi">
+          <p className="text-sobremi">Soy Matías, oriundo de Buenos Aires, Argentina, mejor conocida como &apos; la ciudad de la furia &apos;. Estudiante de la carrera Desarrollo de Software y autodidacta en Desarrollo Web Frontend. Mis principales herramientas/habilidades son HTML, Css, Javascript y actualmente estoy comenzando a aprender / implementar en proyectos la libreria React Js.
+          </p>
+            <p className="text-sobremi">
+              Me considero una persona innovadora, que siempre encuentra la mejor manera de optimizar al maximo la experiencia del usuario.
+            Mi enfoque combina precisión técnica y creatividad  para garantizar resultados
+            de calidad. 
+              ¡Estoy listo para colaborar en tu próximo proyecto y convertir tus ideas en realidad!
+            </p>
+          </div>
+      </section>
     </>;
 }
 
