ded17438e93d15b22cfb7a503136f00b21906a19
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-01-24 12:30:42 -0300

    Agregue url de cada seccion en los enlaces del Header y Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 function Header() {
     return <>
     <header>
-        <a href="#" className="enlace">
+        <a href="https://matiasealianelli.github.io/portafolio-web/#hero" className="enlace">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width={16}
@@ -50,16 +50,16 @@ function Header() {
         d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
     </svg>
         <nav>
-          <a href="#" className="enlace">
+          <a href="https://matiasealianelli.github.io/portafolio-web/#proyectos" className="enlace">
             Proyectos
           </a>
-          <a href="#" className="enlace">
+          <a href="https://matiasealianelli.github.io/portafolio-web/#skills" className="enlace">
             Habilidades
           </a>
-          <a href="#" className="enlace">
+          <a href="https://matiasealianelli.github.io/portafolio-web/#sobreMi" className="enlace">
             Sobre mi
           </a>
-          <a href="#" className="enlace">
+          <a href="https://matiasealianelli.github.io/portafolio-web/#contacto" className="enlace">
             Contacto
           </a>
         </nav>
