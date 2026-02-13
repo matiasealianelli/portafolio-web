4db596191870303c812329306d7e744d35ac9b48
matiasealianelli <hola.matiasalianelli@gmail.com>
2026-02-13 13:45:09 -0300

    Modifique descripcion de seccion Sobre Mi

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,21 +1,37 @@
 import "../styles/AboutMe.css";
 
 function AboutMe() {
-    return <>
-    <section className="sections sobreMi" id="sobreMi">
+  return (
+    <>
+      <section className="sections sobreMi" id="sobreMi">
         <h2 className="title titleSections">Sobre mi</h2>
         <div className="contSobreMi">
-          <p className="text-sobremi">Soy Matías, oriundo de Buenos Aires, Argentina, mejor conocida como &apos; la ciudad de la furia &apos;. Estudiante de la carrera Desarrollo de Software y autodidacta en Desarrollo Web Frontend. Mis principales herramientas/habilidades son HTML, Css, Javascript y actualmente estoy comenzando a aprender / implementar en proyectos la libreria React Js.
+          <p className="text-sobremi">
+            Soy Matías👋 Oriundo de Buenos Aires, Argentina, mejor conocida como
+            &apos; la ciudad de la furia &apos;🤪. Soy Desarrollador Web Full
+            Stack y Diseñador UX/UI en constante aprendizaje; con conocimientos
+            en el stack MERN (MongoDB, Express.js, React, Node.js) para
+            desarrollo.
           </p>
-            <p className="text-sobremi">
-              Me considero una persona innovadora, que siempre encuentra la mejor manera de optimizar al maximo la experiencia del usuario.
-            Mi enfoque combina precisión técnica y creatividad  para garantizar resultados
-            de calidad. 
-              ¡Estoy listo para colaborar en tu próximo proyecto y convertir tus ideas en realidad!
-            </p>
-          </div>
+          <p className="text-sobremi">
+            Actualmente emprendo un negocio en el cual brindo soluciones
+            digitales de diferentes índoles a personas particulares,
+            emprendedores o negocios; garantizando resultados innovadores y de
+            calidad. Enfocado en optimizar la experiencia digital y resolver
+            incidencias técnicas en entornos virtuales.
+          </p>
+        </div>
+        <div>
+          <a
+            target="_blank" className="linkCertificado" title="Ver certificado validado con blockchain"
+            href="https://validator.centrodeelearning.com/validator/R6bmnrrwhu">
+            <i className="bi bi-box-arrow-in-right"></i> Ver certificado
+            Desarrollador Web Full Stack
+          </a>
+        </div>
       </section>
-    </>;
+    </>
+  );
 }
 
 export default AboutMe;
